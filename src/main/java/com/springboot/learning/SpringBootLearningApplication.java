5fd0d103125c619e9f7a42ef062043590a089c54
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-06-17 11:49:15 +0600

    12. save user from main using ApplicationContext

--- src/main/java/com/springboot/learning/SpringBootLearningApplication.java
+++ src/main/java/com/springboot/learning/SpringBootLearningApplication.java
@@ -1,13 +1,27 @@
 package com.springboot.learning;
 
+import com.springboot.learning.entity.User;
+import com.springboot.learning.repositoy.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootLearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLearningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringBootLearningApplication.class, args);
+
+        UserRepository userRepository = context.getBean(UserRepository.class);
+
+        User user = new User();
+        user.setName("Mohibur");
+        user.setCity("Comilla");
+        user.setStatus("I am a Java Developer");
+
+        user = userRepository.save(user);
+
+        System.out.println(user);
+    }
 
 }
