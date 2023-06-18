c5cc4ecb10e682fe0a0262d99acca0f1a6ab2f7e
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-06-19 00:30:34 +0600

    15. jpql & native query

--- src/main/java/com/springboot/learning/SpringBootLearningApplication.java
+++ src/main/java/com/springboot/learning/SpringBootLearningApplication.java
@@ -16,6 +16,9 @@ public class SpringBootLearningApplication {
 
         UserRepository userRepository = context.getBean(UserRepository.class);
 
+        // custom finder
+        System.out.println("Custom finder methods:");
+        System.out.println("______________________");
         List<User> usersByName = userRepository.findByName("Mohibur");
         System.out.println("usersByName: " + usersByName);
 
@@ -24,6 +27,27 @@ public class SpringBootLearningApplication {
 
         List<User> usersByNameAndCity = userRepository.findByNameAndCity("Mohibur", "comilla");
         System.out.println("usersByNameAndCity: " + usersByNameAndCity);
+
+        // jpql
+        System.out.println("JPQL: Java Persistence Query Language");
+        System.out.println("_____________________________________");
+        List<User> allUsers = userRepository.getAllUsers();
+        System.out.println("allUsers:");
+        allUsers.forEach(System.out::println);
+
+        List<User> usersByNameJpql = userRepository.getUsersByName("Mohibur");
+        System.out.println("usersByNameJpql:");
+        usersByNameJpql.forEach(System.out::println);
+
+        List<User> usersByNameAndCityJpql = userRepository.getUsersByNameAndCity("Mohibur", "comilla");
+        System.out.println("usersByNameAndCityJpql:");
+        usersByNameAndCityJpql.forEach(System.out::println);
+
+        // native query
+        System.out.println("Native query");
+        System.out.println("____________");
+        System.out.println("Users by native query:");
+        userRepository.getUsers().forEach(System.out::println);
     }
 
 }
--- src/main/java/com/springboot/learning/repositoy/UserRepository.java
+++ src/main/java/com/springboot/learning/repositoy/UserRepository.java
@@ -1,7 +1,9 @@
 package com.springboot.learning.repositoy;
 
 import com.springboot.learning.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +11,24 @@ import java.util.List;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+    // custom finder
     List<User> findByName(String name);
 
     List<User> findByCity(String city);
 
     List<User> findByNameAndCity(String name, String city);
+
+    // jpql
+    @Query("select u from User u")
+    List<User> getAllUsers();
+
+    @Query("select u from User u where u.name = :n")
+    List<User> getUsersByName(@Param("n") String name);
+
+    @Query("select u from User u where u.name = :n and u.city = :c")
+    List<User> getUsersByNameAndCity(@Param("n") String name, @Param("c") String city);
+
+    // native query
+    @Query(value = "select * from user", nativeQuery = true)
+    List<User> getUsers();
 }
